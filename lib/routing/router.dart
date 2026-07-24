c8c50071793e82a6a57495981c43803110f1a692
adarsh-kishore786 <adarsh.kishore786@gmail.com>
2026-07-24 09:57:07 +0530

    Removing history for now, will add it back later

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -4,7 +4,6 @@ import 'package:go_router/go_router.dart';
 import 'package:momentum/notifiers/project_tab_reset_notifier.dart';
 import 'package:momentum/routing/routes.dart';
 import 'package:momentum/screens/dashboard_screen.dart';
-import 'package:momentum/screens/history_screen.dart';
 import 'package:momentum/screens/project_screen.dart';
 
 class MomentumFooter extends ConsumerWidget {
@@ -15,26 +14,25 @@ class MomentumFooter extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isHistory = currentPath == Routes.history;
-    
+   
     return Scaffold(
       body: child,
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: isHistory ? 1 : 0,
-        onTap: (index) {
-          if (index == 1 && !isHistory) {
-            context.push(Routes.history);
-          } else if (index == 0) {
-            if (isHistory) {
-              context.canPop() ? context.pop() : context.go(Routes.dashboard);
-            } else {
-              ref.read(projectTabResetProvider.notifier).state++;
-            }
-          }
-        },
+        // currentIndex: isHistory ? 1 : 0,
+        // onTap: (index) {
+        //   if (index == 1 && !isHistory) {
+        //     context.push(Routes.history);
+        //   } else if (index == 0) {
+        //     if (isHistory) {
+        //       context.canPop() ? context.pop() : context.go(Routes.dashboard);
+        //     } else {
+        //       ref.read(projectTabResetProvider.notifier).state++;
+        //     }
+        //   }
+        // },
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.grid_view), label: 'Projects'),
-          BottomNavigationBarItem(icon: Icon(Icons.history), label: 'History'),
+          BottomNavigationBarItem(icon: Icon(Icons.more_horiz), label: 'More')
         ],
       ),
     );
@@ -51,9 +49,6 @@ final routerProvider = Provider<GoRouter>((ref) {
           StatefulShellBranch(routes: [
             GoRoute(path: Routes.dashboard, builder: (_, _) => DashboardScreen()),
           ]),
-          StatefulShellBranch(routes: [
-            GoRoute(path: Routes.history, builder: (_, _) => const HistoryScreen()),
-          ])
         ]
       ),
       GoRoute(
